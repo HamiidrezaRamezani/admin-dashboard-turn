2476c2e860e5e4c355b673a433508364fb77a607
hamidreza <hamiidreza0121@gmail.com>
2024-11-24 10:05:32 +0330

    add reports boxes

--- lib/reports.dart
+++ lib/reports.dart
@@ -13,7 +13,7 @@ class _ReportsState extends State<Reports> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
+        child: ListView(
           children: [
             const Row(
               children: [
@@ -27,17 +27,154 @@ class _ReportsState extends State<Reports> {
             const SizedBox(
               height: 24.0,
             ),
-            // Expanded(
-            //     child: ),
+            Padding(
+              padding: const EdgeInsets.only(left: 36.0, right: 36.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF628DFF),
+                        borderRadius: BorderRadius.all(Radius.circular(12.0))
+                      ),
+                      child: const Padding(padding: EdgeInsets.all(24.0), child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text('15', style: TextStyle(
+                              fontFamily: "bold",
+                              fontSize: 56.0,
+                              color: Colors.white
+                            ),),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Text('بلیط های گرفته شده', style: TextStyle(fontSize: 16.0, fontFamily: "regular", color: Colors.white),),
+                          )
+                        ],
+                      ),),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 36.0,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      decoration: const BoxDecoration(
+                          color: Color(0xFF58fcec),
+                          borderRadius: BorderRadius.all(Radius.circular(12.0))
+                      ),
+                      child: const Padding(padding: EdgeInsets.all(24.0), child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text('36', style: TextStyle(
+                                fontFamily: "bold",
+                                fontSize: 56.0,
+                                color: Colors.white
+                            ),),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Text('کاربران ثبت نام شده', style: TextStyle(fontSize: 16.0, fontFamily: "regular", color: Colors.white),),
+                          )
+                        ],
+                      ),),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             const SizedBox(
               height: 36.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 36.0, right: 36.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      decoration: const BoxDecoration(
+                          color: Color(0xFF303A2B),
+                          borderRadius: BorderRadius.all(Radius.circular(12.0))
+                      ),
+                      child: const Padding(padding: EdgeInsets.all(24.0), child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text('خدمات پر استفاده', style: TextStyle(fontSize: 16.0, fontFamily: "regular", color: Colors.white),),
+                            ],
+                          ),
+                          SizedBox(height: 14.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('آموزش غواصی 100', style: TextStyle(fontSize: 12.0, fontFamily: "regular", color: Colors.white),),
+                              Text('پدل 100', style: TextStyle(fontSize: 12.0, fontFamily: "regular", color: Colors.white),),
+                            ],
+                          ),
+                          SizedBox(height: 12.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('شاتل سواری 100', style: TextStyle(fontSize: 12.0, fontFamily: "regular", color: Colors.white),),
+                              Text('اسکی روی آب 50', style: TextStyle(fontSize: 12.0, fontFamily: "regular", color: Colors.white),),
+                            ],
+                          ),
+                          SizedBox(height: 12.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('بنانا سواری 250', style: TextStyle(fontSize: 12.0, fontFamily: "regular", color: Colors.white),),
+                            ],
+                          )
+                        ],
+                      ),),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 36.0,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      decoration: const BoxDecoration(
+                          color: Color(0xFFFF99C9),
+                          borderRadius: BorderRadius.all(Radius.circular(12.0))
+                      ),
+                      child: const Padding(padding: EdgeInsets.all(24.0), child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text('500.000', style: TextStyle(
+                                fontFamily: "bold",
+                                fontSize: 56.0,
+                                color: Colors.white
+                            ),),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Text('کل پرداختی', style: TextStyle(fontSize: 16.0, fontFamily: "regular", color: Colors.white),),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text('تومان', style: TextStyle(fontSize: 16.0, fontFamily: "regular", color: Colors.white),),
+                          )
+                        ],
+                      ),),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-        },
+        onPressed: () {},
         label: const Text(
           'دریافت فایل Excel',
           style: TextStyle(
